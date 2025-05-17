539db9c7d881a9cf42ec7d56e7fad580d3cf5b91
Haroon Saifi <haroonsaifi2001@gmail.com>
2025-05-17 20:52:53 +0530

    experiments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import rehypePrettyCode from 'rehype-pretty-code'
+import react from '@astrojs/react'
+import { transformerCopyButton } from '@rehype-pretty/transformers'
+import tailwindcss from '@tailwindcss/vite'
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), icon()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'rose-pine',
+          transformers: [
+            transformerCopyButton({
+              visibility: 'hover',
+              feedbackDuration: 2_500,
+            }),
+          ],
+        },
+      ],
+    ],
+  },
+})
